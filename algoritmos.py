09b59e0dbcee7b39a73d6a2a38dfa743cf094914
Jesus Salas <jesuse.salas@alumnos.udg.mx>
2020-10-12 21:00:09 -0500

    Implementación

--- algoritmos.py
+++ algoritmos.py
@@ -1,4 +1,8 @@
+import math
+
 def distancia_euclidiana(x_1, y_1, x_2, y_2):
     distancia = math.sqrt((x_2 - x_1)**2 + (y_2 - y_1)**2)
 
-    return distancia    
+    print(distancia)
+
+distancia_euclidiana(500, 500, 200, 200)
